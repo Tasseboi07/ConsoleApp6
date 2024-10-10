50a19f252074e30004e54865e7c024ddf8e578dd
johe07003 <johe07003@E-PW05C37X>
2024-10-10 12:44:35 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace hej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång hoppade Alma");
+            string längd=Console.ReadLine();
+            int I = int.Parse(längd);
+            Console.WriteLine("Hur Mycket längre hoppade elin?");
+            string Y = Console.ReadLine();
+            int J = int.Parse(Y);
+            double x = J + I;
+            Console.WriteLine("Elin hoppade" +x);
+            
+             
+        }
+    }
+}
